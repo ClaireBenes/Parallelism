77a0c0197d5568591f0126c421bcbd475171ebfa
Benes Claire <claire.benes@artfx.fr>
2026-01-09 14:49:27 +0100

    [add] messages
    struct of :
    - Order
    - IngredientRequest
    - MealReady

---
+++ TestThread/Source/Restaurant/Messages.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <future>
+
+struct Order
+{
+    int customerId;
+    int ingredients[3];
+    std::promise<void>* mealPromise = nullptr;
+};
+
+
+struct IngredientRequest
+{
+    int ingredientId;
+};
+
+struct MealReady
+{
+    int customerId;
+};
+
