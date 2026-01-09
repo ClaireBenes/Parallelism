a55f54dbb4d2650eb74b72d64a446e1d5fee04a8
Benes Claire <claire.benes@artfx.fr>
2026-01-09 14:48:55 +0100

    [add] message queue

---
+++ TestThread/Source/Restaurant/MessageQueue.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template<typename T>
+class MessageQueue
+{
+public:
+    void push(const T& value)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            queue.push(value);
+        }
+        cv.notify_one();
+    }
+
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cv.wait(lock, [&] { return !queue.empty(); });
+
+        T value = queue.front();
+        queue.pop();
+        return value;
+    }
+
+private:
+    std::queue<T> queue;
+    std::mutex mutex;
+    std::condition_variable cv;
+};
